0cdf68f8818c2a4bb8236be38352fa3c86a6bc12
jaswanthvarma06641 <jaswanthvarma06641@gmail.com>
2024-07-01 20:01:14 +0530

    port

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+// import "./homeStyles.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="background_image content_align">
+        <section className="landing_page_content">
+          <div className="intro_text">
+            <h3>Hii!</h3>
+            <h3>I'm Ghanath Vootukuru</h3>
+            <p>
+              Cybersecurity enthusiast with a knack for innovation and strategic
+              solutions.
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Home;
